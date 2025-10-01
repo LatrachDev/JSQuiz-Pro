90cbd9d12409ad530717acaf635280dbdebd54ac
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-10-01 10:52:17 +0100

    database connection

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+const db = require('./config/database');
+
+
+const app = express()
+
+
+
+app.get("/", (req, res) => {
+    res.send('hi')
+})
+app.listen(3000)
