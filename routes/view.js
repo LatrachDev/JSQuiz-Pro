846c340413d1d44348cd73ac2a28dc6f83358946
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-10-03 11:25:34 +0100

    themes view

--- routes/view.js
+++ routes/view.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const viewController = require('../controllers/viewController');
 
 router.get('/', viewController.home);
+router.get('/themes', viewController.themes);
 
 module.exports = router;
