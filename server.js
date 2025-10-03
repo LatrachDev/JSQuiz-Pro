d8e831079acff2ccaf929d95f1416966fd009dbb
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-10-03 11:37:19 +0100

    x

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // static files
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("public"));
 // Default layout file
 app.use(expressLayouts);
